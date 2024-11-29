b30e34528343ab405dd7211f7678f45df02bc1d3
Mazen <mazenadel19@gmail.com>
2024-11-29 22:28:41 +0200

    feat: add firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,63 @@
+'use client'
+
+import { auth } from '@/utils/firebase'
+import { onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from 'firebase/auth'
+import { useEffect, useState } from 'react'
+
 export default function Home() {
-    return <div>Hello World</div>
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+
+    async function login() {
+        try {
+            console.log('login')
+            const userCredentials = await signInWithEmailAndPassword(auth, email, password)
+            console.log(userCredentials)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function register() {
+        try {
+            console.log('register')
+            const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+            console.log(userCredentials)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function logout() {
+        try {
+            console.log('logout')
+            await signOut(auth)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const uid = user.uid
+                console.log(uid, user)
+                // ...
+            } else {
+                // User is signed out
+                // ...
+                console.log('signed out')
+            }
+        })
+    }, [])
+    return (
+        <div>
+            <h1>Home</h1>
+            <button onClick={login}>Login</button>
+            <button onClick={register}>Register</button>
+            <button onClick={logout}>Logout</button>
+            <input type="text" onChange={(e) => setEmail(e.target.value)} />
+            <input type="text" onChange={(e) => setPassword(e.target.value)} />
+        </div>
+    )
 }
